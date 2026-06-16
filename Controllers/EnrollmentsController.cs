e6be9339c2de605fed9aa4af5a2f161a8004aad7
kalu <kaluabiyu@gmail.com>
2026-06-16 07:57:49 +0300

    Adding The course,student,assessment API

--- Controllers/EnrollmentsController.cs
+++ Controllers/EnrollmentsController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace TmsApi.Controllers;
+
 
 [ApiController]
 [Route("api/enrollments")]
--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fda424be04fcf8803a7f4f0631433cf1650d21f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f634140fc92192d7484557786f3fd94489e3e497")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
