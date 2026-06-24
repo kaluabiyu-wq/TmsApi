7d1fcee8316298a6a442832cb111aac3b4500399
kalu <kaluabiyu@gmail.com>
2026-06-24 21:34:26 +0300

    N + 1 pattern with shaping

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8221c8abfe69ac38b82288c5702c404de67783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925c8cb9899e888dbf3dddc9723531ec4a049204")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
