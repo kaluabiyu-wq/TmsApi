c876f4984d7eba92c98c13cd0e714d77a3804399
kalu <kaluabiyu@gmail.com>
2026-06-25 07:56:38 +0300

    separate interface and services and add to new folder interface

--- Service/CourseService.cs
+++ Service/CourseService.cs
@@ -1,12 +1,5 @@
 
 
-public interface ICourseService
-{
-    Task<CourseRecord> CreateAsync(string title, int capacity);
-    Task<CourseRecord?> GetByIdAsync(string id);
-    Task<IReadOnlyList<CourseRecord>> GetAllAsync();
-    Task<bool> DeleteAsync(string id);
-}
 
 public class CourseService : ICourseService
 {
--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e517e636c504b482365054d8221fb5fd08eeb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f2a9daeb17bb8b9ffd3856d161aa7bf496d4f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
