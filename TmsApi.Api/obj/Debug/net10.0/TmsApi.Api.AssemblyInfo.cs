b5fb01315f1264b2dd5759fc9b57ec3f6432a542
kalu <kaluabiyu@gmail.com>
2026-07-18 20:54:12 +0300

    Api Versioning and Deprecation Strategy

--- TmsApi.Api/obj/Debug/net10.0/TmsApi.Api.AssemblyInfo.cs
+++ TmsApi.Api/obj/Debug/net10.0/TmsApi.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34272c4828f73403ba37978d84df134b51bf1d60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed119bd42d95a4f556fab147f3c7286ebb9836e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
