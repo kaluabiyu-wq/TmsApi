697e270caba611debcb63e89ac97681d22bdfc8d
kalu <kaluabiyu@gmail.com>
2026-08-17 16:03:08 +0300

    add authcontroller in v1

--- TmsApi.Application/obj/Debug/net10.0/TmsApi.Application.AssemblyInfo.cs
+++ TmsApi.Application/obj/Debug/net10.0/TmsApi.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e88d9a0bc96ef3261ce879c0ec7478217373ba07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87bc2d3979cbcbcc73783ffcefff5098baacba57")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
