34b91bb0cc19cd49c47a1e41c5bb07fab52cf4b4
kalu <kaluabiyu@gmail.com>
2026-09-04 14:17:06 +0300

    remove unused courseController

--- TmsApi.Api/obj/Debug/net10.0/TmsApi.Api.AssemblyInfo.cs
+++ TmsApi.Api/obj/Debug/net10.0/TmsApi.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6592553a4064fe2e0ae65aef6e6c1356e67bbd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e30d71ac396c3c7264d883f259bb443a4ba7a9d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
